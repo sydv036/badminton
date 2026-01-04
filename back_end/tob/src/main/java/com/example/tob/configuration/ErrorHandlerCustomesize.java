69079ed012f8947ec3a17bf135c8f5bd275bfe61
sydv <dangvansy412003@gmail.com>
2026-01-04 20:31:14 +0700

    config/sydv: update version springboot

--- back_end/tob/src/main/java/com/example/tob/configuration/ErrorHandlerCustomesize.java
+++ back_end/tob/src/main/java/com/example/tob/configuration/ErrorHandlerCustomesize.java
@@ -13,8 +13,33 @@ public class ErrorHandlerCustomesize implements ResponseErrorHandler {
         return false;
     }
 
+    /**
+     * Handle the error in the given response.
+     * <p>This method is only called when {@link #hasError(ClientHttpResponse)}
+     * has returned {@code true}.
+     *
+     * @param response the response with the error
+     * @throws IOException in case of I/O errors
+     */
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+
+    }
+
+    /**
+     * Alternative to {@link #handleError(ClientHttpResponse)} with extra
+     * information providing access to the request URL and HTTP method.
+     *
+     * @param url      the request URL
+     * @param method   the HTTP method
+     * @param response the response with the error
+     * @throws IOException in case of I/O errors
+     * @since 5.0
+     */
     @Override
     public void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
         ResponseErrorHandler.super.handleError(url, method, response);
     }
+
+
 }
---
+++ back_end/tob/src/main/java/com/example/tob/constans/DateTimeConstans.java
@@ -0,0 +1,14 @@
+package com.example.tob.constans;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class DateTimeConstans {
+
+    private static final String DTF_YYYYMMDD = "yyyyMMdd";
+
+    private static final String DTF_YYYYMMDDHHMMSS = "yyyymmmddHHmmss";
+
+}
