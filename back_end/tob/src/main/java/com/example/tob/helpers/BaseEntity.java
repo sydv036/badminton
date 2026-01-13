1d95e7e3ba295a655e4aab3daadd16cd58a90835
sydv <dangvansy412003@gmail.com>
2026-01-13 23:53:09 +0700

    feat/sydv/config: update auditorAware of Spring

--- back_end/tob/src/main/java/com/example/tob/helpers/BaseEntity.java
+++ back_end/tob/src/main/java/com/example/tob/helpers/BaseEntity.java
@@ -1,16 +1,17 @@
 package com.example.tob.helpers;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public abstract class BaseEntity {
@@ -19,12 +20,14 @@ public abstract class BaseEntity {
     private UUID publicId;
 
     @Column(nullable = false, updatable = false)
+    @CreatedBy
     private String createdBy;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @Column(nullable = false)
+    @LastModifiedBy
     private String updatedBy;
 
     @Column(nullable = false)
